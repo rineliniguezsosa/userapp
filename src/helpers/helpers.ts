8e4a9ab92c3b669488dc46679476a7b758fd2846
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 17:52:54 -0600

    [FEAT] Estableciendo todas las columnas para la información de mis datos en la table

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -8,13 +8,31 @@ export const columns: GridColDef[] = [
         field:'name', headerName: 'Name',width:70
     },
     {
-        field:'genero', headerName: 'Genero',width:70
+        field:'genre', headerName: 'Gener',width:70
+    },
+    {
+        field:'streetNumber', headerName: 'N.calle',width:70
+    },
+    {
+        field:'streetName', headerName: 'Nombre calle',width:70
+    },
+    {
+        field:'streetCity', headerName: 'Ciudad',width:70
+    },
+    {
+        field:'streetState', headerName: 'Estado',width:70
+    },
+    {
+        field:'streetCountry', headerName: 'País',width:70
+    },
+    {
+        field:'stretPostcode', headerName: 'Codigo postal',width:70
     },
     {
         field:'email', headerName: 'email',width:70
     },
     {
-        field:'nat', headerName: 'Nacionalidad',width:70
+        field:'nat', headerName: 'nacionalidad',width:70
     },
     
 ]
