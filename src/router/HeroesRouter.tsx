aa4533fceed7ef0809a04038c40a536e809a7aec
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 18:53:09 -0600

    [FEAT] Estableciendo en las rutas el componente Home
    - importando el componente Home
    - importando el componente Routes para definir las rutas
    - importando el componente Route para definir la ruta en donde la ruta principal devolveremos el componente Home

--- src/router/HeroesRouter.tsx
+++ src/router/HeroesRouter.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { Home } from "../components/feature/heroes/pages/Home"
 
 
 export const HeroesRouter = () => {
   return (
     <BrowserRouter>
-    
+        <Routes>
+            <Route path="/" Component={Home}></Route>
+        </Routes>
     </BrowserRouter>
   )
 }
