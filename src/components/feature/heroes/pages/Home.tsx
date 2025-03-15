72186e335a7a64b4f9ce8fe8e4c6c5b858c04e44
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 13:48:39 -0600

    [FEAT] Multiples cambios en el componente Home.tsx, el proceso ya es de consumir la api rest y obtener a los usuarios
    - importando el hook useState para almacenar la información de la api
    - importando axios para realizar solicitudes http
    - importando la interface UsuarioRequest para tipar nuestra respuesta
    - creando la variable getall para obtener nuestra url de nuestra petición
    - creando el hook userdata para tipar e inicializar mi state
    - log de userdata
    - creé el método asincrono getUsers en donde estableci el bloque try catch , creé la variable req para esperar la respuesta en obtener todos los usuarios
    - creé la variable resp donde esperé la obtención de la data
    - estableci un log para ver la información
    - actualice el state de usedata mediante la función setUserData para obtener y almacenar el valor de la solicitud
    - estableci logs para ver errores
    - en mi función useEffect llame al método getUsers

--- src/components/feature/heroes/pages/Home.tsx
+++ src/components/feature/heroes/pages/Home.tsx
@@ -1,12 +1,30 @@
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
+import axios from 'axios';
+import { UsuarioRequest } from "../../../../types/interfaces";
+const getall = import.meta.env.VITE_GETALL;
+
 
 
 export const Home = () => {
+  const [userdata, setUserData] = useState<UsuarioRequest | []>([])
+  console.log(userdata);
+  
+  const getUsers = async() =>{
+    try {
+      const req = await axios.get<UsuarioRequest>(getall);
+      const resp = await req.data;
+      console.log(resp);
+      setUserData(resp)
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
 
   useEffect(()=>{
-
+    getUsers()
   },[])
-  
+
   return (
     <div className="w-full h-screen border-2 flex border-blue-600">
         {/* contenido de mi api, lista de usuarios  */}
