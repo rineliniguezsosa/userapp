8c884cbb297db64292886007c8a824f67680a7dd
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 18:59:54 -0600

    [FEAT] Estableciendo el nav en mi componente HeroesRouter
    - importando el componente Nav

--- src/router/HeroesRouter.tsx
+++ src/router/HeroesRouter.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import { Home } from "../components/feature/heroes/pages/Home"
+import { Nav } from "../components/layout/nav/Nav"
 
 
 export const HeroesRouter = () => {
   return (
     <BrowserRouter>
+        <Nav/>
         <Routes>
             <Route path="/" Component={Home}></Route>
         </Routes>
