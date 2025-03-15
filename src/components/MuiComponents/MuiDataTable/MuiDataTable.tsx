e1170f3232e6476300e3d30ecc9ac56154c7feb0
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 15:31:19 -0600

    [FEAT] Cambios en el componente MuiDataTable
    - desestructurando la prop checkboxSelection y asignandole el valor true
    - estableciendo la prop checkboxSelection en el componente DataGrid

--- src/components/MuiComponents/MuiDataTable/MuiDataTable.tsx
+++ src/components/MuiComponents/MuiDataTable/MuiDataTable.tsx
@@ -1,10 +1,12 @@
 import { DataGrid } from '@mui/x-data-grid';
 import { MuiDataTaBleProps } from '../../../types/interfaces';
 
-export const MuiDataTable = ({columns,...props}:MuiDataTaBleProps) => {
+export const MuiDataTable = ({rows,columns,checkboxSelection = true,...props}:MuiDataTaBleProps) => {
   return (
     <DataGrid
+       rows={rows}
        columns={columns}
+       checkboxSelection={checkboxSelection}
        {...props}
     />
   )
