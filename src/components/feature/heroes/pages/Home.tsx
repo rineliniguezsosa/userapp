6604623b5dbe0a1f52ca47401285e9e928d33895
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 19:39:02 -0600

    [FEAT] Cambios de css en el Home
    - estableci css en el div inicial en donde estableci un width full y height-screen con un color de borde
    - estableci en el 2do div un width del 80% con un color y tamaño de borde
    - estableci al 3er div un width del 20% con un tamaño y color de borde

--- src/components/feature/heroes/pages/Home.tsx
+++ src/components/feature/heroes/pages/Home.tsx
@@ -2,14 +2,14 @@
 
 export const Home = () => {
   return (
-    <div>
+    <div className="w-full h-screen border-2 border-blue-600">
         {/* contenido de mi api, lista de heroes */}
-        <div>
+        <div className="w-4/5 border-2 border-red-500">
 
         </div>
 
         {/* form,agregar nuevas items */}
-        <div>
+        <div className="w-1/5 border-2 border-red-500">
 
         </div>
     </div>
