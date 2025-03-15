074e2c56e0f3b2d6c8c7c32c4fe1f2ae9ef09736
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 19:59:48 -0600

    [FEAT] Estableciendo la ruta para editar la información de mis heroes
    - importe el componente UpdateHeroes
    - estableci la ruta con el path "/edit/heroe" para redireccionar mediante un botón hacia la actualización , devolvi el componente UpdateHeroesv

--- src/router/HeroesRouter.tsx
+++ src/router/HeroesRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes,Route,Navigate } from "react-router-dom"
 import { Home } from "../components/feature/heroes/pages/Home"
 import { Nav } from "../components/layout/nav/Nav"
+import { UpdateHeroes } from "../components/feature/heroes/pages/UpdateHeroes"
 
 
 export const HeroesRouter = () => {
@@ -10,6 +11,7 @@ export const HeroesRouter = () => {
             <Nav/>
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
+                <Route path="/edit/heroe" element={<UpdateHeroes />}></Route>
 
                 <Route path="/*" element={<Navigate to="/"/>}></Route>
             </Routes>
