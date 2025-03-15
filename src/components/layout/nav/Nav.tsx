72373c25ebcb2ab78ee4d0b705b7313855d4a432
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 18:49:37 -0600

    [FEAT] Pegandole mi contenido de mi nav de mi repositorio anterior

--- src/components/layout/nav/Nav.tsx
+++ src/components/layout/nav/Nav.tsx
@@ -1,6 +1,15 @@
+import { NavLink } from "react-router-dom"
+import Brightness5Icon from '@mui/icons-material/Brightness5';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
 
 export const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="w-full border-2 border-red-500 p-5 flex justify-between">
+        <NavLink to="/">Home</NavLink>
+
+        <div>
+          <Brightness5Icon/> / <ModeNightIcon/>
+        </div>
+    </nav>
   )
 }
