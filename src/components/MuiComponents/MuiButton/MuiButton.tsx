5d381177b9ab898ecd6b742843365aff319c01de
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 13:37:48 -0600

    [FEAT] Desestructurando la prop loading en el componente MuiButton

--- src/components/MuiComponents/MuiButton/MuiButton.tsx
+++ src/components/MuiComponents/MuiButton/MuiButton.tsx
@@ -1,9 +1,10 @@
 import Button from '@mui/material/Button';
 import { MuiButtonProps } from '../../../types/interfaces';
 
-export const MuiButton = ({variant,color,onClick,sizes,children,...props}:MuiButtonProps) => {
+export const MuiButton = ({loading,variant,color,onClick,sizes,children,...props}:MuiButtonProps) => {
   return (
     <Button
+        loading={loading}
         variant={variant}
         color={color}
         onClick={onClick}
