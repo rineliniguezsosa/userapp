9a2e71eb08eb8daba6744579dfb18cc5718b49e9
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 21:16:59 -0600

    [FEAT] Cambios en el componente MuiModal
    - importando el componente Box
    - estableciendo la prop style con las propiedades css de la documentación oficial de material ui
    - estableciendo el componente Box en donde estableci el atributo sx para aplicar css en donde propague las propiedades css de style

--- src/components/MuiComponents/MuiModal/MuiModal.tsx
+++ src/components/MuiComponents/MuiModal/MuiModal.tsx
@@ -1,11 +1,26 @@
-import { Modal } from "@mui/material"
+import { Box, Modal } from "@mui/material"
 import { MuiModalProps } from "../../../types/interfaces"
 
 
 export const MuiModal = ({open,onClose,children,...props}:MuiModalProps) => {
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        pt: 2,
+        px: 4,
+        pb: 3,
+    };
   return (
     <Modal open={open} onClose={onClose} {...props}>
-        {children}
+        <Box sx={{...style}}>
+            {children}
+        </Box>
     </Modal>
   )
 }
