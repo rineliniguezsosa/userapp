935a50fd014c7384135ee979dd35d814b632b1c3
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 17:07:46 -0600

    [FEAT] Cambios en el componente MuiTextField
    - imoportando la interface  MuiTextFieldProps
    - desestructurando las props y tipando las props pro la interface  MuiTextFieldProps
    - estableciendo las props en el componente TextField y estableciendo la prop variant como outlined para que sea predefinida

--- src/components/MuiComponents/MuiTextField/MuiTextField.tsx
+++ src/components/MuiComponents/MuiTextField/MuiTextField.tsx
@@ -1,9 +1,13 @@
 import TextField from '@mui/material/TextField';
+import { MuiTextFieldProps } from '../../../types/interfaces';
 
-export const MuiTextField = () => {
+export const MuiTextField = ({id,label,value}:MuiTextFieldProps) => {
   return (
     <TextField
-    
+      id={id}
+      label={label}
+      value={value}
+      variant='outlined'
     />
   )
 }
