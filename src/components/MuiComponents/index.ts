105746d67ed24a3343b0fafc6f9016225c5f3bd9
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 16:51:31 -0600

    [FEAT] Exportando todo del archivo de barril en MuiForm

--- src/components/MuiComponents/index.ts
+++ src/components/MuiComponents/index.ts
@@ -2,3 +2,4 @@ export * from './MuiPaper/MuiPaper';
 export * from './MuiDataTable/MuiDataTable'
 export * from './MuiButton/MuiButton';
 export * from './MuiModal/MuiModal';
+export * from './MuiForm/MuiForm';
