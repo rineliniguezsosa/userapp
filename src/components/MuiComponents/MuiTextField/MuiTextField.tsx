eda79673408cadb45ea4b07474adebb24246f152
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 17:34:05 -0600

    [FEAT] Cambios en el componente MuiTextField
    - estableciendo la prop onChange y desestructurado la prop onChange

--- src/components/MuiComponents/MuiTextField/MuiTextField.tsx
+++ src/components/MuiComponents/MuiTextField/MuiTextField.tsx
@@ -1,13 +1,14 @@
 import TextField from '@mui/material/TextField';
 import { MuiTextFieldProps } from '../../../types/interfaces';
 
-export const MuiTextField = ({id,label,value,...props}:MuiTextFieldProps) => {
+export const MuiTextField = ({id,label,value,onChange,...props}:MuiTextFieldProps) => {
   return (
     <TextField
       id={id}
       label={label}
       value={value}
       variant='outlined'
+      onChange={onChange}
       {...props}
     />
   )
