4a1a2f02032ff22131baeb2fb5368a5cdb3b6689
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 17:10:21 -0600

    [FEAT] Propagando las props de mi componente TextField

--- src/components/MuiComponents/MuiTextField/MuiTextField.tsx
+++ src/components/MuiComponents/MuiTextField/MuiTextField.tsx
@@ -1,13 +1,14 @@
 import TextField from '@mui/material/TextField';
 import { MuiTextFieldProps } from '../../../types/interfaces';
 
-export const MuiTextField = ({id,label,value}:MuiTextFieldProps) => {
+export const MuiTextField = ({id,label,value,...props}:MuiTextFieldProps) => {
   return (
     <TextField
       id={id}
       label={label}
       value={value}
       variant='outlined'
+      {...props}
     />
   )
 }
