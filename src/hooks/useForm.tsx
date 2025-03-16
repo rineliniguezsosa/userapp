80961ff0a16988b723a140d8c5539161f54ef8f8
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 17:23:09 -0600

    [FEAT] Cambios en el useForm
    - importando el useState para actualizar mis propiedades dinamicas
    - creandome la interface generica para el tipado de mis propiedades con clave y valor
    - estableciendo el parametro initialForm en donde obtendremos el valor inicial del formulario

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
 
+interface FormState<T> {
+    [key: string]: T;
+}
 
-export const useForm = () =>{
+export const useForm = <T extends FormState<string>>(initialForm:T) =>{
+    console.log(initialForm);
+    
     return {
 
     }
