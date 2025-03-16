1478f8cd2121f1175d1fb2c0b57a6a7eb24ca5f9
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 17:46:32 -0600

    [FEAT} Cambios en el componente MuiTextField
    - desestructurando el name y pasando la prop name al componente TextField

--- src/components/MuiComponents/MuiTextField/MuiTextField.tsx
+++ src/components/MuiComponents/MuiTextField/MuiTextField.tsx
@@ -1,11 +1,12 @@
 import TextField from '@mui/material/TextField';
 import { MuiTextFieldProps } from '../../../types/interfaces';
 
-export const MuiTextField = ({id,label,value,onChange,...props}:MuiTextFieldProps) => {
+export const MuiTextField = ({id,label,name,value,onChange,...props}:MuiTextFieldProps) => {
   return (
     <TextField
       id={id}
       label={label}
+      name={name}
       value={value}
       variant='outlined'
       onChange={onChange}
