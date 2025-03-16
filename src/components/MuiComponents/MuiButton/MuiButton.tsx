dab67273e1ce4c80e739de2df875256c79086229
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 21:48:39 -0600

    [FEAT] Estableciendo mi prop onClick en el componente MuiButton para poder realizar acciones

--- src/components/MuiComponents/MuiButton/MuiButton.tsx
+++ src/components/MuiComponents/MuiButton/MuiButton.tsx
@@ -1,11 +1,12 @@
 import Button from '@mui/material/Button';
 import { MuiButtonProps } from '../../../types/interfaces';
 
-export const MuiButton = ({variant,color,sizes,children,...props}:MuiButtonProps) => {
+export const MuiButton = ({variant,color,onClick,sizes,children,...props}:MuiButtonProps) => {
   return (
     <Button
         variant={variant}
         color={color}
+        onClick={onClick}
         size={sizes}
         {...props}
     >
