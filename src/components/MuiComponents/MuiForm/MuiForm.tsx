0c4b1a77e700ad2efc74fcc1e722dd23e9100dbe
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 21:32:42 -0600

    [FEAT] Cambios en el componente MuiForm
    - desestructurando la prop title
    - estableciendo el elemento h1 con las props css font-bold font-sans y paso la prop title para que en el formulario aparezca el title de mi formulario

--- src/components/MuiComponents/MuiForm/MuiForm.tsx
+++ src/components/MuiComponents/MuiForm/MuiForm.tsx
@@ -1,9 +1,10 @@
 import { Box } from '@mui/material';
 import { MuiFormProps } from '../../../types/interfaces';
 
-export const MuiForm = ({children,onSubmit,...props}:MuiFormProps) => {
+export const MuiForm = ({children,onSubmit,title,...props}:MuiFormProps) => {
   return (
     <Box sx={{width:'100%',border:'1px red solid',display:'flex',flexDirection:'column',gap:2}} onSubmit={onSubmit} component="form" autoComplete='off' {...props}>
+        <h1 className="font-bold font-sans">{title}</h1>
         {children}
     </Box>
   )
