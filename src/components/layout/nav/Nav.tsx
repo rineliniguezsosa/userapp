a35219e22ae0442b5ba4d263f2e417b09d5a1ff8
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 21:07:22 -0600

    [FEAT] Eliminando la importación de los iconos Brightness5Icon y ModeNightIcon
    - eliminando los iconos

--- src/components/layout/nav/Nav.tsx
+++ src/components/layout/nav/Nav.tsx
@@ -1,15 +1,11 @@
 import { NavLink } from "react-router-dom"
-import Brightness5Icon from '@mui/icons-material/Brightness5';
-import ModeNightIcon from '@mui/icons-material/ModeNight';
+
 
 export const Nav = () => {
   return (
     <nav className="w-full border-2 border-red-500 p-5 flex justify-between">
         <NavLink className="" to="/">Usuarios CRUD</NavLink>
 
-        <div>
-          <Brightness5Icon/> / <ModeNightIcon/>
-        </div>
     </nav>
   )
 }
