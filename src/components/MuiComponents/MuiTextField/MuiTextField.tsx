ded299141ab8831af5242c863f54e22699aed72a
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 18:08:44 -0600

    [FEAT] Cambios en el componente MuiTextField
    - desestructurando la prop children y select, si la prop select no viene sera por default false
    - estableciendo el children para el componente MenuItem

--- src/components/MuiComponents/MuiTextField/MuiTextField.tsx
+++ src/components/MuiComponents/MuiTextField/MuiTextField.tsx
@@ -1,7 +1,7 @@
 import TextField from '@mui/material/TextField';
 import { MuiTextFieldProps } from '../../../types/interfaces';
 
-export const MuiTextField = ({id,label,name,value,onChange,...props}:MuiTextFieldProps) => {
+export const MuiTextField = ({id,label,name,value,onChange,children,select = false,...props}:MuiTextFieldProps) => {
   return (
     <TextField
       id={id}
@@ -10,7 +10,10 @@ export const MuiTextField = ({id,label,name,value,onChange,...props}:MuiTextFiel
       value={value}
       variant='outlined'
       onChange={onChange}
+      select={select}
       {...props}
-    />
+    >
+      {children}
+    </TextField>
   )
 }
