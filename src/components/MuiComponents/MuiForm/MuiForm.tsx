40b11ae1ec7852e9d76a1b89607e300ab363a43c
Rinel <rineliniguezsosa@gmail.com>
2025-03-16 21:09:28 -0600

    [FEAT] Estableciendo props css en el componente Box
    - width del 100%
    - tamaño y color de borde
    - use display flex para alinear los elementos a column
    - use el gap a 2rem

--- src/components/MuiComponents/MuiForm/MuiForm.tsx
+++ src/components/MuiComponents/MuiForm/MuiForm.tsx
@@ -3,7 +3,7 @@ import { MuiFormProps } from '../../../types/interfaces';
 
 export const MuiForm = ({children,onSubmit,...props}:MuiFormProps) => {
   return (
-    <Box onSubmit={onSubmit} component="form" autoComplete='off' {...props}>
+    <Box sx={{width:'100%',border:'1px red solid',display:'flex',flexDirection:'column',gap:2}} onSubmit={onSubmit} component="form" autoComplete='off' {...props}>
         {children}
     </Box>
   )
