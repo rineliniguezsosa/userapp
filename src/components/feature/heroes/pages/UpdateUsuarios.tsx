763bb7c2a75094fbd9c66a587db017594ae95e9b
Rinel <rineliniguezsosa@gmail.com>
2025-03-18 00:21:13 -0600

    [FEAT] multiples cambios en el componente UpdateHeroes
    - eliminando la importación de useState
    - importando el componente MuiButton
    - comentando mi state y el log de user
    - desestructurando la referencia de la función setform
    - eliminando el log de user
    - desestructurando info después de un analisis el método de mi backend me devuelve un array de objetos por lo que para mostrar la información en los inputs ó componentes MuiTextField deben de ser unos objetos
    - estableci un log para ver info
    - use mi función setForm para actualizar la data de mi form en donde estableci una estructura diferente para mostrar la información ó equivalente a mi state inicial
    - estableci mis multiples componentes MuiTextField para mostrar la información de mi usuario
    - estableci mi componente MuiButton con el titulo de actualizar

--- src/components/feature/heroes/pages/UpdateUsuarios.tsx
+++ src/components/feature/heroes/pages/UpdateUsuarios.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom"
-import { MuiForm, MuiTextField } from "../../../MuiComponents";
-import { Usuario } from "../../../../types/interfaces";
+import { MuiButton, MuiForm, MuiTextField } from "../../../MuiComponents";
+// import { Usuario } from "../../../../types/interfaces";
 import { useForm } from "../../../../hooks";
 const getuserbyid = import.meta.env.VITE_USERBYID;
 
 export const UpdateHeroes = () => {
-  const [user, setuser] = useState<Usuario[]>([])
-  const {form,updateUser,handleChange} = useForm({
+  // const [user, setuser] = useState<Usuario[]>([])
+  // console.log(user);
+  
+  const {form,setform,updateUser,handleChange} = useForm({
       name:'',
       gender:'',
       streetNumber:'',
@@ -21,7 +23,6 @@ export const UpdateHeroes = () => {
       email:'',
       nat:''
     })
-  console.log(user);
   
   const location = useLocation();
   // const navigate = useNavigate();
@@ -33,9 +34,21 @@ export const UpdateHeroes = () => {
   const getUserByMongoId = async() =>{
     try {
       const { data } = await axios.get(`${getuserbyid}/${_id}`)
-      console.log(data.data);
-      setuser(data.data)
-      
+      const [ info ] = data.data;
+      console.log(info);
+       
+      setform({
+        name: info.name || '',
+        gender: info.gender || '',
+        streetNumber: info.location?.street?.number?.toString() || '',
+        streetName: info.location?.street?.name || '',
+        city: info.location?.city || '',
+        state: info.location?.state || '',
+        country: info.location?.country || '',
+        postcode: info.location?.postcode?.toString() || '',
+        email: info.email || '',
+        nat: info.nat || ''
+      })
     } catch (error) {
       console.log(error);
       
@@ -45,12 +58,94 @@ export const UpdateHeroes = () => {
   useEffect(()=>{
     getUserByMongoId();
   },[_id])
-  
-  
+    
   return (
     <div className="w-full m-auto">
       <MuiForm title="Actualizar registros" onSubmit={updateUser} >
-      <h1>jjj</h1>
+          <MuiTextField
+            id="name"
+            label="Nombre" 
+            name="name"
+            type="text"
+            value={form.name}  
+            onChange={handleChange}         
+          />
+
+          <MuiTextField
+            id="streetNumber"
+            label="N.Calle" 
+            name="streetNumber"
+            type="text"
+            value={form.streetNumber}  
+            onChange={handleChange}            
+          />
+
+          <MuiTextField
+            id="streetName"
+            label="Nombre de calle" 
+            name="streetName"
+            type="text"
+            value={form.streetName}  
+            onChange={handleChange}            
+          />
+
+          <MuiTextField
+            id="city"
+            label="Cuidad" 
+            name="city"
+            type="text"
+            value={form.city}  
+            onChange={handleChange}            
+          />
+
+          <MuiTextField
+            id="state"
+            label="Estado" 
+            name="state"
+            type="text"
+            value={form.state}  
+            onChange={handleChange}            
+          />
+
+          <MuiTextField
+            id="country"
+            label="País" 
+            name="country"
+            type="text"
+            value={form.country}  
+            onChange={handleChange}            
+          />
+
+          <MuiTextField
+            id="postcode"
+            label="Codigo postal" 
+            name="postcode"
+            type="text"
+            value={form.postcode}  
+            onChange={handleChange}            
+          />
+
+          <MuiTextField
+            id="email"
+            label="Email" 
+            name="email"
+            type="email"
+            value={form.email}  
+            onChange={handleChange}            
+          />
+
+          <MuiTextField
+            id="nat"
+            label="Nacionalidad" 
+            name="nat"
+            type="text"
+            value={form.nat}  
+            onChange={handleChange}         
+          />
+          
+          <MuiButton type="submit" variant="contained" color="success" sizes="medium">
+                Actualizar
+          </MuiButton>
       </MuiForm>
     </div>
   )
