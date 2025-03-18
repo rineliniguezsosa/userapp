052b7cfc99edf882583417fc502d95574193d013
Rinel <rineliniguezsosa@gmail.com>
2025-03-18 00:34:43 -0600

    [FEAT] Eliminando tamaño y color de bordes

--- src/components/layout/nav/Nav.tsx
+++ src/components/layout/nav/Nav.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 
 export const Nav = () => {
   return (
-    <nav className="w-full border-2 border-red-500 p-5 flex justify-between">
+    <nav className="w-full p-5 flex justify-between">
         <NavLink className="" to="/">Usuarios CRUD</NavLink>
 
     </nav>
