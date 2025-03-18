339a6c3e6cf6a7a78afdb494bcc4aa5a61e24b79
Rinel <rineliniguezsosa@gmail.com>
2025-03-17 18:41:38 -0600

    [FEAT] Cambios en el componente UpdateHeroes
    - importando el hook useLocation para obtener el query param y información de la pagina
    - creando la variable location en donde estableci la referencia del hook useLocation
    - estableciendo el log de location para observar la información

--- src/components/feature/heroes/pages/UpdateUsuarios.tsx
+++ src/components/feature/heroes/pages/UpdateUsuarios.tsx
@@ -1,6 +1,10 @@
+import { useLocation } from "react-router-dom"
 
 
 export const UpdateHeroes = () => {
+  const location = useLocation();
+  console.log(location);
+  
   return (
     <div>UpdateHeroes</div>
   )
