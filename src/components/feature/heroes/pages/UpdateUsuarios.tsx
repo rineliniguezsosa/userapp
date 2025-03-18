96341de1a6ea390c39b7b4804b0e34ce9b68d651
Rinel <rineliniguezsosa@gmail.com>
2025-03-17 18:47:39 -0600

    [FEAT] Cambios en el componente UpdateHeroes
    - creé la variable queryParams en donde me creé la instancia URLSearchParams para obtener los parametros
    - estableci un log para ver el query param "_id"

--- src/components/feature/heroes/pages/UpdateUsuarios.tsx
+++ src/components/feature/heroes/pages/UpdateUsuarios.tsx
@@ -3,7 +3,10 @@ import { useLocation } from "react-router-dom"
 
 export const UpdateHeroes = () => {
   const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
   console.log(location);
+  console.log("q",queryParams.get("_id"));
+  
   
   return (
     <div>UpdateHeroes</div>
