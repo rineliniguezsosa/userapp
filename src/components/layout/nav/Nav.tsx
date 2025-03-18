9ebabab2a8157de14a241f7acba4f5bdc614c27f
Rinel <rineliniguezsosa@gmail.com>
2025-03-18 13:01:07 -0600

    [FEAT] Estableciendo la ruta home

--- src/components/layout/nav/Nav.tsx
+++ src/components/layout/nav/Nav.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom"
 export const Nav = () => {
   return (
     <nav className="w-full p-5 flex justify-between">
-        <NavLink className="" to="/">Usuarios CRUD</NavLink>
+        <NavLink className="" to="/home">Usuarios CRUD</NavLink>
 
     </nav>
   )
