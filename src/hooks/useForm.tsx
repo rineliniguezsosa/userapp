804163bd7babc622b6cdb4968a3b47748cf27a37
Rinel <rineliniguezsosa@gmail.com>
2025-03-17 20:19:06 -0600

    [FEAT] Me pego las mismas validaciones en el método updateUser

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -109,6 +109,79 @@ export const useForm = (initialForm:FormState) =>{
 
     const updateUser = async(event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+
+        const {name, streetNumber, streetName, city, state, country, postcode, email, nat } = form;
+            const isValid = !name || !streetNumber || !streetName || !city || !state || !country || !postcode || !email || !nat
+            
+            if(isValid){
+                alert("Todos los campos son obligatorios");
+                return;
+            }
+
+            if(!/[A-Za-z]/.test(name)){
+                alert("El nombre solo debe de incluir letras");
+                return ;
+            }
+
+            if(!/[0-9]+/.test(streetNumber)){
+                alert('El campo número de calle debe de incluir solo números');
+                return ; 
+            }
+            
+            if(!/[A-Za-z]/.test(streetName)){
+                alert("El campo nombre de calle solo debe de incluir letras");
+                return ;
+            }
+
+            if(!/[A-Za-z]/.test(city)){
+                alert("El campo ciudad solo debe de incluir letras");
+                return ;
+            }
+
+            if(!/[A-Za-z]/.test(state)){
+                alert("El campo estado solo debe de incluir letras");
+                return ;
+            }
+
+            if(!/[A-Za-z]/.test(country)){
+                alert("El campo país solo debe de incluir letras");
+                return ;
+            }
+
+            if(!/[0-9]{4}$/.test(postcode)){
+                alert("El campo codigo postal no cumple con los requisitos");
+                return ;
+            }
+
+            // eslint-disable-next-line no-useless-escape
+            if(!/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(email)){
+                alert("El campo email no cumple con los requisitos");
+                return ;
+            }
+
+            if(!/^[A-Za-z]{2}$/.test(nat)){
+                alert("El campo nacionalidad debe de incluir una abreviatura de 2 letras: México : Mx")
+                return ;
+            }
+
+            const data = {
+                name:name,
+                gender:'female',
+                location:{
+                    street:{
+                        number: streetNumber,
+                        name: streetName
+                    },
+                    city:city,
+                    state:state,
+                    country:country,
+                    postcode:postcode
+                },
+                email:email,
+                nat:nat
+            }
+            console.log(data);
+            
     }
     
     return {
