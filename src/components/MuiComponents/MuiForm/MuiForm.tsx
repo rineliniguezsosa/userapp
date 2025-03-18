22cbed579e510978a0519fe8ca4e525db52b10e2
Rinel <rineliniguezsosa@gmail.com>
2025-03-18 00:30:34 -0600

    [FEAT] Desestructurando la prop className y estableciendola en el componente Box para añadir tailwindcss

--- src/components/MuiComponents/MuiForm/MuiForm.tsx
+++ src/components/MuiComponents/MuiForm/MuiForm.tsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material';
 import { MuiFormProps } from '../../../types/interfaces';
 
-export const MuiForm = ({children,onSubmit,title,...props}:MuiFormProps) => {
+export const MuiForm = ({children,onSubmit,title,className,...props}:MuiFormProps) => {
   return (
-    <Box sx={{width:'100%',border:'1px red solid',display:'flex',flexDirection:'column',gap:2}} onSubmit={onSubmit} component="form" autoComplete='off' {...props}>
+    <Box className={className} onSubmit={onSubmit} component="form" autoComplete='off' {...props}>
         <h1 className="font-bold font-sans">{title}</h1>
         {children}
     </Box>
   )
 }
+// sx={{width:'100%',border:'1px red solid',display:'flex',flexDirection:'column',gap:2}}
