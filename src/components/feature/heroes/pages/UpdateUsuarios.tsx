4aaab15da1993113dc629fcb38b2f3c497263c73
Rinel <rineliniguezsosa@gmail.com>
2025-03-17 18:59:09 -0600

    [FEAT] Multiples cambios en el componente UpdateHeroes
    - importando useEffect
    - importando useNavigate
    - creando la variable navigate en donde pase la referencia de la función useNavigate
    - creé la variable _id en donde obtengo el query param _id
    - creé el método getUserByMongoId  en donde obtengo el _id de mongo y estblezco el bloque try catch para verificar la solicitud http
    - me creé la fiunción useEffect en donde estableci bajo dependencias el _id de mongo y el navigate , si existe mi _id de mongo llamo al método getUserByMongoId y si no existe un _id navegaré al usuario hacia el inicio

--- src/components/feature/heroes/pages/UpdateUsuarios.tsx
+++ src/components/feature/heroes/pages/UpdateUsuarios.tsx
@@ -1,11 +1,31 @@
-import { useLocation } from "react-router-dom"
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import { useLocation,useNavigate } from "react-router-dom"
 
 
 export const UpdateHeroes = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
+  const _id = queryParams.get("_id");
   console.log(location);
   console.log("q",queryParams.get("_id"));
+  
+  const getUserByMongoId = (_id:string) =>{
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(()=>{
+    if(_id){
+      getUserByMongoId(_id);
+    }else{
+      navigate('/')
+    }
+  },[_id,navigate])
   
   
   return (
