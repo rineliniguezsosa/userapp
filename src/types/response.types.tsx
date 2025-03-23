3e4802dc8ccc389025ad37c0beb973d8b9d530fe
Rinel <rineliniguezsosa@gmail.com>
2025-03-23 17:32:03 -0600

    [FEAT] Creando la interface AllCountries en donde tipe la respuesta de country state city api

--- src/types/response.types.tsx
+++ src/types/response.types.tsx
@@ -1,3 +1,14 @@
+export interface AllCountries {
+    id:        number;
+    name:      string;
+    iso2:      string;
+    iso3:      string;
+    phonecode: string;
+    capital:   string;
+    currency:  string;
+    native:    null | string;
+    emoji:     string;
+}
 export interface Restcountries {
     name:         Name;
     tld?:         string[];
